db3142ed6e4cff0dd1fa8bf16b949fd7b8acfd0e
rijwan-uddin <rijwan35-653@diu.edu.bd>
2024-07-30 18:56:25 +0600

    piechart update

--- lib/pages/report_page.dart
+++ lib/pages/report_page.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class ReportPage extends StatelessWidget {
@@ -10,6 +11,38 @@ class ReportPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Report'),
       ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              width: 300,
+              height: 300,
+              child: PieChart(
+                PieChartData(
+                  sections: generateSections(
+                    
+                  )
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
+
+ List<PieChartSectionData> generateSections() {
+    return List.generate(4, (index) => PieChartSectionData(
+      title: 'Title ${index + 1}',
+      showTitle: true,
+      radius: 70.0,
+      value: 25.0,
+      color: Color.fromARGB(255, 150, 20 * index, 30 * index),
+      titleStyle: TextStyle(
+        fontSize: 16.0,
+        fontWeight: FontWeight.bold,
+      )
+
+    ));
+ }
 }
