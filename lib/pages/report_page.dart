8914d14b93bf64a850925055b2c5b6954cd0cf85
rijwan-uddin <rijwan35-653@diu.edu.bd>
2024-08-17 18:24:05 +0600

    Line chart (static)

--- lib/pages/report_page.dart
+++ lib/pages/report_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 class ReportPage extends StatelessWidget {
   static const String routeName = 'report';
+
   const ReportPage({super.key});
 
   @override
@@ -17,13 +18,7 @@ class ReportPage extends StatelessWidget {
             SizedBox(
               width: 300,
               height: 300,
-              child: PieChart(
-                PieChartData(
-                  sections: generateSections(
-                    
-                  )
-                ),
-              ),
+              child: buildLineChart(),
             )
           ],
         ),
@@ -31,18 +26,48 @@ class ReportPage extends StatelessWidget {
     );
   }
 
- List<PieChartSectionData> generateSections() {
-    return List.generate(4, (index) => PieChartSectionData(
-      title: 'Title ${index + 1}',
-      showTitle: true,
-      radius: 70.0,
-      value: 25.0,
-      color: Color.fromARGB(255, 150, 20 * index, 30 * index),
-      titleStyle: TextStyle(
-        fontSize: 16.0,
-        fontWeight: FontWeight.bold,
-      )
+  PieChart buildPieChart() {
+    return PieChart(
+      PieChartData(sections: generateSections()),
+    );
+  }
 
-    ));
+  List<PieChartSectionData> generateSections() {
+    return List.generate(
+        4,
+        (index) => PieChartSectionData(
+            title: 'Title ${index + 1}',
+            showTitle: true,
+            radius: 70.0,
+            value: 25.0,
+            color: Color.fromARGB(255, 150, 20 * index, 30 * index),
+            titleStyle: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+            )));
+  }
+
+ LineChart buildLineChart() {
+    return LineChart(
+      LineChartData(
+        minX: 0,
+        minY: 0,
+        maxX: 11,
+        maxY: 6,
+        lineBarsData: [
+
+          LineChartBarData(
+            spots: [
+
+            ],
+            isCurved: true,
+            isStrokeCapRound: true,
+            barWidth: 4,
+            color: Colors.amber,
+          )
+
+        ],
+      )
+    );
  }
 }
