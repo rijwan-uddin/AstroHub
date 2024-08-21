93bfe1ce56432c8bdad7e6d1d452fbc5e4c76284
rijwan-uddin <rijwan35-653@diu.edu.bd>
2024-08-21 20:14:35 +0600

    code fixed

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+
       builder: EasyLoading.init(),
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
